baebe9c88ef9f38fe92de4914b25d937d9c9cea1
Nikit <mixxator47@gmail.com>
2020-02-14 16:27:29 +0800

    первые наброски сцены кликера(закладки)

---
+++ ClickDeff/Assets/Scripts/changeBookmarks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeBookmarks : MonoBehaviour
+{
+    public GameObject target;
+    private void OnMouseDown()
+    {
+        Debug.Log("click works");
+        transform.parent.parent.gameObject.SetActive(false);
+        target.SetActive(true);
+    }
+}
